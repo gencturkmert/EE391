8b66929b795160002db0e58a3ceab3cd6b8d7fdc
Mert <gencturkmert1@gmail.com>
2023-11-13 22:58:50 +0300

    fixes

--- matlab_assignment_1/square_wave.m
+++ matlab_assignment_1/square_wave.m
@@ -1,36 +1,34 @@
 x = @(t) -1*(t>=0 & t<1) + 1*(t>=1 & t<2);
 
+% Plot original signal
 figure;
-plot(t_period, x(mod(t_period,2)), 'b-', 'LineWidth', 2);
+t = linspace(-4, 4, 400);
+T0 = 2;
+plot(t, x(mod(t_period,4)), 'b-', 'LineWidth', 2);
 title('Original Signal');
 xlabel('Time');
 ylabel('Amplitude');
-ylim([-1.5, 1.5]);
+ylim([-2, 2]);
 
-for kmax = 2:2:12
+for kmax = 12:2:12
     k_values = 1:kmax;
     
-    t_period = linspace(-4, 4, 400);
-    
-    x_reconstructed = zeros(size(t_period));
-    a0 = integral(@(t) x(t), 0, 2) / 2;
+    x_reconstructed = zeros(size(t));
+    a0 = integral(@(t) x(t), 0, T0) / T0;
     x_reconstructed = x_reconstructed + a0;
     for idx = 1:length(k_values)
         k = k_values(idx);
     
-        an_integral = @(t) x(t).*cos(2*pi*k*t/2);
-        bn_integral = @(t) x(t).*sin(2*pi*k*t/2);
-    
-        an = integral(an_integral, 0, 2) / 2;
-        bn = integral(bn_integral, 0, 2) / 2;
-    
-        x_reconstructed = x_reconstructed + (an*cos(2*pi*k*t_period/2) + bn*sin(2*pi*k*t_period/2)) * 2/pi;
+        an = integral(@(t) x(t) .* exp(-1i * 2*t* pi * k / 4),0,T0) / T0;
+        bn = integral(@(t) x(t) .* exp(-1i * 2*t* pi * -k / 4),0,T0) / T0;
+        x_reconstructed = x_reconstructed + an * exp(1i * 2* pi*t * k / 4) + bn * exp(1i * 2* pi*t * -k / 4);
     end
     
+    % Plot reconstructed signal in a new figure for each iteration
     figure;
     plot(t_period, real(x_reconstructed), 'r--', 'LineWidth', 2);
     title(['Reconstructed Signal (k = ', num2str(kmax), ')']);
     xlabel('Time');
     ylabel('Amplitude');
-    ylim([-1.5, 1.5]);
+    ylim([-2, 2]);
 end
--- matlab_assignment_1/triangle_wave.m
+++ matlab_assignment_1/triangle_wave.m
@@ -1,35 +1,34 @@
 x = @(t) (t >= 0 & t < 2) .* t - ((t >= 2 & t < 4) .* (-4 + t));
+% Plot original signal
 figure;
-plot(t_period, x(mod(t_period,4)), 'b-', 'LineWidth', 2);
+t = linspace(-8, 8, 400);
+T0 = 4;
+plot(t, x(mod(t_period,4)), 'b-', 'LineWidth', 2);
 title('Original Signal');
 xlabel('Time');
 ylabel('Amplitude');
 ylim([0, 2]);
 
-for kmax = 2:2:12
+for kmax = 12:2:12
     k_values = 1:kmax;
     
-    t_period = linspace(-8, 8, 400);
     
-    x_reconstructed_piecewise = zeros(size(t_period));
-    a0 = integral(@(t) x(t), 0, 4) / 4;
-    x_reconstructed_piecewise = x_reconstructed_piecewise + a0;
+    x_reconstructed = zeros(size(t));
+    a0 = integral(@(t) x(t), 0, T0) / T0;
+    x_reconstructed = x_reconstructed + a0;
     for idx = 1:length(k_values)
         k = k_values(idx);
     
-        an_integral = @(t) x(t).*cos(2*pi*k*t/4);
-        bn_integral = @(t) x(t).*sin(2*pi*k*t/4);
-    
-        an = integral(an_integral, 0, 4) / 2;
-        bn = integral(bn_integral, 0, 4) / 2;
-    
-        x_reconstructed_piecewise = x_reconstructed_piecewise + (an*cos(2*pi*k*t_period/4) + bn*sin(2*pi*k*t_period/4)) * 2/pi;
+        an = integral(@(t) x(t) .* exp(-1i * 2*t* pi * k / 4),0,T0) / T0;
+        bn = integral(@(t) x(t) .* exp(-1i * 2*t* pi * -k / 4),0,T0) / T0;
+        x_reconstructed = x_reconstructed + an * exp(1i * 2* pi*t * k / 4) + bn * exp(1i * 2* pi*t * -k / 4);
     end
     
+    % Plot reconstructed signal in a new figure for each iteration
     figure;
-    plot(t_period, real(x_reconstructed_piecewise), 'r--', 'LineWidth', 2);
+    plot(t_period, real(x_reconstructed), 'r--', 'LineWidth', 2);
     title(['Reconstructed Signal (k = ', num2str(kmax), ')']);
     xlabel('Time');
     ylabel('Amplitude');
-    ylim([0, 2]);
+    ylim([-1, 3]);
 end
