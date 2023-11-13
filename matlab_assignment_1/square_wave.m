c325de68a2eb6c3f58ab76badd386e0b0a327252
Mert <gencturkmert1@gmail.com>
2023-11-13 23:23:45 +0300

    square wave fin

--- matlab_assignment_1/square_wave.m
+++ matlab_assignment_1/square_wave.m
@@ -2,9 +2,9 @@ x = @(t) -1*(t>=0 & t<1) + 1*(t>=1 & t<2);
 
 % Plot original signal
 figure;
-t = linspace(-4, 4, 400);
+t = linspace(-3, 3, 400);
 T0 = 2;
-plot(t, x(mod(t_period,4)), 'b-', 'LineWidth', 2);
+plot(t, x(mod(t,T0)), 'b-', 'LineWidth', 2);
 title('Original Signal');
 xlabel('Time');
 ylabel('Amplitude');
@@ -13,20 +13,21 @@ ylim([-2, 2]);
 for kmax = 2:2:12
     k_values = 1:kmax;
     
+    
     x_reconstructed = zeros(size(t));
     a0 = integral(@(t) x(t), 0, T0) / T0;
     x_reconstructed = x_reconstructed + a0;
     for idx = 1:length(k_values)
         k = k_values(idx);
     
-        an = integral(@(t) x(t) .* exp(-1i * 2*t* pi * k / 4),0,T0) / T0;
-        bn = integral(@(t) x(t) .* exp(-1i * 2*t* pi * -k / 4),0,T0) / T0;
-        x_reconstructed = x_reconstructed + an * exp(1i * 2* pi*t * k / 4) + bn * exp(1i * 2* pi*t * -k / 4);
+        an = integral(@(t) x(t) .* exp(-1i * 2*t* pi * k / T0),0,T0) / T0;
+        bn = integral(@(t) x(t) .* exp(-1i * 2*t* pi * -k / T0),0,T0) / T0;
+        x_reconstructed = x_reconstructed + an * exp(1i * 2* pi*t * k / T0) + bn * exp(1i * 2* pi*t * -k / T0);
     end
     
     % Plot reconstructed signal in a new figure for each iteration
     figure;
-    plot(t_period, real(x_reconstructed), 'r--', 'LineWidth', 2);
+    plot(t, real(x_reconstructed), 'r--', 'LineWidth', 2);
     title(['Reconstructed Signal (k = ', num2str(kmax), ')']);
     xlabel('Time');
     ylabel('Amplitude');
